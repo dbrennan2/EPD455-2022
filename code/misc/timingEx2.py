4a73e95ebb9fc86cf6898dc0d2d1702c4f7eea30
DanNegrut <negrut@wisc.edu>
2022-06-23 17:29:52 -0500

    small stuff

--- code/misc/timingEx2.py
+++ code/misc/timingEx2.py
@@ -1,33 +1,18 @@
-import timeit
-
-# ONE APPROACH
-# setup code
-import_math = "import math"
-
-# code snippet
-code = """
-def sum(a,b,c):
-	tmp1 = math.sqrt(a) + math.atan(b) + math.sin(c)
-	tmp2 = math.sqrt(a+1.0) + math.atan(b+2.0) + math.sin(c+0.13)
-	return tmp1/tmp2
-"""
-# execute code above 100000 times
-howManyTimes = 100000
-execution_time = timeit.repeat(stmt=code, setup=import_math, number=howManyTimes)
-print("Total Execution time:", execution_time, "seconds (for", howManyTimes, "runs)")
-
-# ANOTHER APPROACH
+# ANOTHER APPROACH FOR TIMING A FUNCTION
 # Here you can even pass the value of the arguments
+import timeit
 import math
 import functools
 
-def sum(a,b,c):
-	tmp1 = math.sqrt(a) + math.atan(b) + math.sin(c)
-	tmp2 = math.sqrt(a+1.0) + math.atan(b+2.0) + math.sin(c+0.13)
-	return tmp1/tmp2
+
+def sum(a, b, c):
+    tmp = math.sqrt(a) + math.atan(b) + math.sin(c)
+    return tmp**2
+
 
 aval = 0.1
 bval = 0.3
 cval = 0.04
-timeTaken = timeit.Timer(functools.partial(sum, aval, bval, cval)) 
+timeTaken = timeit.Timer(functools.partial(sum, aval, bval, cval))
+howManyTimes = 100000
 print(timeTaken.timeit(howManyTimes))
