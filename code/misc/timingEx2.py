a7898f4db7514ad4881959cfe577eb5091fb8c0c
Huzaifg <huzaifa.unjha@gmail.com>
2022-08-30 18:18:11 -0500

    extra example. more comments

--- code/misc/timingEx2.py
+++ code/misc/timingEx2.py
@@ -13,6 +13,7 @@ def sum(a, b, c):
 aval = 0.1
 bval = 0.3
 cval = 0.04
+# callable created using functools and passed to the timer
 timeTaken = timeit.Timer(functools.partial(sum, aval, bval, cval))
 howManyTimes = 100000
 print(timeTaken.timeit(howManyTimes))
