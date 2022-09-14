169f0fe1bfa106b0859edb490575ce1207c16629
Dan Negrut <negrut@wisc.edu>
2022-09-14 17:13:18 -0500

    small changes

--- code/timing/timingEx3.py
+++ code/timing/timingEx3.py
@@ -1,30 +1,33 @@
 import time
+import argparse
 
 def approach1():
     """works, but not preferred"""
-    sum = 0
-    # start time
-    tic = time.time()
-    # piece of code
+    sum = 0    
+    tic = time.time() # start watch
     for i in range(100000):
         sum+=i
-    # end time
-    toc = time.time()
+    toc = time.time() # stop watch
     print(f"Elapsed time: {toc - tic:0.7f} s.")
 
 def approach2():
     """works, and  preferred"""
     sum = 0
-    # start time
-    tic = time.perf_counter()
-    # piece of code
+    tic = time.perf_counter() # start watch
     for i in range(100000):
-        sum+=i
-    # end time
-    toc = time.perf_counter()
+        sum+=i    
+    toc = time.perf_counter() # stop watch
     print(f"Elapsed time: {toc - tic:0.7f} s with perf_counter.")
 
 
 if __name__ == "__main__":
-    approach1()
-    #approach2()   
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c','--choice', type = int, required = True, \
+        help='Select 1 or 2 to experiment with two different types of timing.')
+    args = parser.parse_args()
+    if args.choice==1:
+        approach1()
+    elif args.choice==2:
+        approach2()
+    else:
+        print("argument list no ok.")
