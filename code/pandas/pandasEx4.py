b8dac046fb3c7a73b73bcbd579ffb565a77d0598
Dan Negrut <negrut@wisc.edu>
2022-10-02 16:05:20 -0500

    small...

--- code/pandas/pandasEx4.py
+++ code/pandas/pandasEx4.py
@@ -1,15 +1,13 @@
 import pandas as pd
-
-# this data can also be from a csv file or any other file
-data = {
-    'Jason' : [4,4,3,2], # Each column name is given by the key
+movieData = {
+    'Jason' : [4,4,3,2], 
     'Harry' : [2,1,4,4],
     'Bibi'  : [5,5,5,5],
     'Thomas': [1,5,5,3]
 }
-index = ['Rogue One','Guardians of the Galaxy','Manchester by the Sea','La La Land']
-# create a dataframe from the data
-df = pd.DataFrame(data = data, index = index)
+titles = ['Rogue One','Guardians of the Galaxy','Manchester by the Sea','La La Land']
+df = pd.DataFrame(data = movieData, index = titles)
+print(df)
 
 # ilocate 'Rogue One' row
 print(df.iloc[0])
--- code/pandas/pandasEx5.py
+++ code/pandas/pandasEx5.py
@@ -1,17 +1,12 @@
 import pandas as pd
-
-# this data can also be from a csv file or any other file
-data = {
-    'Jason' : [4,4,3,2], # Each column name is given by the key
+movieData = {
+    'Jason' : [4,4,3,2], 
     'Harry' : [2,1,4,4],
     'Bibi'  : [5,5,5,5],
     'Thomas': [1,5,5,3]
 }
-index = ['Rogue One','Guardians of the Galaxy',\
-    'Manchester by the Sea','La La Land']
-    
-# create a dataframe from the data
-df = pd.DataFrame(data = data, index = index)
+titles = ['Rogue One','Guardians of the Galaxy','Manchester by the Sea','La La Land']
+df = pd.DataFrame(data = movieData, index = titles)
 
 # Get row wise average and store it in a column
 df.loc[:,'Movie - Avg. rating'] = df.mean(axis=1)
@@ -19,6 +14,6 @@ df.loc[:,'Movie - Avg. rating'] = df.mean(axis=1)
 # Get column wise average and store it in a row
 df.loc['Person - Avg. rating'] = df.mean(axis=0)
 
-# set the last element to 0 - it has no meaning
-df.iloc[-1,-1] = 0
-print(df)
+# set the last element to None since it has no meaning
+df.iloc[-1,-1] = None
+df.to_csv('movieRating.csv')
