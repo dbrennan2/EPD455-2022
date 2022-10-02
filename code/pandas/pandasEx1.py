cdb702f27be1c9faef19bf648a543b460e15499f
Dan Negrut <negrut@wisc.edu>
2022-10-02 14:42:17 -0500

    small

--- code/pandas/pandasEx1.py
+++ code/pandas/pandasEx1.py
@@ -1,15 +1,16 @@
 import pandas as pd
 
-# this data can also be from a csv file or any other file
-data = {
-    'Jason' : [4,4,3,2], # Each column name is given by the key
+# data could also be from csv or other format file
+movieData = {
+    # Each column name is given by the key
+    'Jason' : [4,4,3,2], 
     'Harry' : [2,1,4,4],
     'Bibi'  : [5,5,5,5],
     'Thomas': [1,5,5,3]
 }
 
 # create a dataframe from the data
-df = pd.DataFrame(data = data)
+df = pd.DataFrame(data = movieData)
 
 # print the data
 print(df)
